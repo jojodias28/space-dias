fae72ebf3ef71a849a25dbb261a7f39b42b2da11
jojodias28 <dias.jo1992@gmail.com>
2024-10-30 20:57:49 -0300

    Feat: feat navbar component

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,24 @@
+import "./style.css";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <h1>Space Dias</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
